9df5c4264c542f3d206ed2a00877bb2e02ef6556
“pne” <jenkinsactia2023@gmail.com>
2023-07-04 13:19:44 +0200

    Secodn Commit;

--- src/example/MainExample.java
+++ src/example/MainExample.java
@@ -3,7 +3,8 @@ package example;
 public class MainExample {
 
 	public static void main(String[] args) {
-		System.out.println( "Erste example with SSH" );
+		System.out.println( "Erste example with SSH"  );
+		System.out.println( "zweiter example with SSH"  );
 
 	}
 
