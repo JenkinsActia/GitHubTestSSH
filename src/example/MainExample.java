72c0a933839b39123d3333f4e4b0a0b6fe55e44b
“pne” <jenkinsactia2023@gmail.com>
2023-07-05 10:00:57 +0200

    Dritte malll

--- src/example/MainExample.java
+++ src/example/MainExample.java
@@ -5,6 +5,7 @@ public class MainExample {
 	public static void main(String[] args) {
 		System.out.println( "Erste example with SSH"  );
 		System.out.println( "zweiter example with SSH"  );
+		System.out.println( "Dritte example with SSH??????"  );
 
 	}
 
